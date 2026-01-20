26310833dd906835ffa70f93875473c73cfdeab7
TheophileBoissondeChazournes <theophile.boisson_de_chazournes@etu.minesparis.psl.eu>
2026-01-20 17:00:23 +0100

    fin premiere seance

--- interface/plateau.py
+++ interface/plateau.py
@@ -1,6 +1,5 @@
 import pyxel
-
-
+from PIL import Image
 
 class Plateau:
     def __init__(self):
@@ -12,21 +11,28 @@ class Plateau:
         self.offset_y = (250 - total_size) // 2
         self.pieces = [[None for _ in range(8)] for _ in range(8)]
         for i in range(8):
-            self.pieces[1][i] = 'p'  
-            self.pieces[6][i] = 'P'  
-        self.pieces[0][0] = self.pieces[0][7] = 't'
-        self.pieces[7][0] = self.pieces[7][7] = 'T'
-        self.pieces[0][1] = self.pieces[0][6] = 'c'
-        self.pieces[7][1] = self.pieces[7][6] = 'C'
-        self.pieces[0][2] = self.pieces[0][5] = 'f'
-        self.pieces[7][2] = self.pieces[7][5] = 'F'
-        self.pieces[0][3] = 'q'
-        self.pieces[7][3] = 'Q'
-        self.pieces[0][4] = 'k'
-        self.pieces[7][4] = 'K'
-     
+            self.pieces[1][i] = 'bP'  
+            self.pieces[6][i] = 'wP' 
+        self.pieces[0][0] = self.pieces[0][7] = 'bR'
+        self.pieces[7][0] = self.pieces[7][7] = 'wR'
+        self.pieces[0][1] = self.pieces[0][6] = 'bN'
+        self.pieces[7][1] = self.pieces[7][6] = 'wN'
+        self.pieces[0][2] = self.pieces[0][5] = 'bB'
+        self.pieces[7][2] = self.pieces[7][5] = 'wB'
+        self.pieces[0][3] = 'bQ'
+        self.pieces[7][3] = 'wQ'
+        self.pieces[0][4] = 'bK'
+        self.pieces[7][4] = 'wK'
+        self.images = {}
+        self.load_images()
         pyxel.run(self.update, self.draw)
-        
+
+    def load_images(self):
+        pieces_files = ["bP","wP","bR","wR","bN","wN","bB","wB","bQ","wQ","bK","wK"]
+        for name in pieces_files:
+            img = Image.open(f"images/{name}.png").resize((self.cell_size, self.cell_size))
+            self.images[name] = pyxel.image(0).load(0, 0, img)    
+    
     def update(self):
         pass
 
@@ -37,26 +43,12 @@ class Plateau:
                 x = self.offset_x + col * self.cell_size
                 y = self.offset_y + ligne * self.cell_size
 
-                # Couleurs alternées
-                if (ligne + col) % 2 == 0:
-                    color = 7  
-                else:
-                    color = 0  
-
+                color = 7 if (ligne + col) % 2 == 0 else 0
                 pyxel.rect(x, y, self.cell_size, self.cell_size, color)
-                
+
                 piece = self.pieces[ligne][col]
                 if piece:
-                    coord_x, coord_y = PIECE_COORD[piece]
-                    pyxel.blt(
-                        x, y,                
-                        0,                  
-                        coord_x * self.cell_size,
-                        coord_y * self.cell_size,
-                        self.cell_size,
-                        self.cell_size,
-                        0                    
-                    )
+                    pyxel.blt(x, y, 0, 0, 0, self.cell_size, self.cell_size, 0)
 
         for col in range(8):
             lettre = chr(ord("A") + col)
@@ -71,19 +63,3 @@ class Plateau:
             pyxel.text(x, y, nb, 0)
 
 Plateau()
-
-
-PIECE_COORD = {
-    'P': (0, 1),  
-    'p': (0, 0),  
-    'T': (1, 1),  
-    't': (1, 0),  
-    'C': (2, 1),  
-    'c': (2, 0),  
-    'F': (3, 1),  
-    'f': (3, 0),  
-    'Q': (4, 1),  
-    'q': (4, 0),  
-    'K': (5, 1),  
-    'k': (5, 0),  
-}
