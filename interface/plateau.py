04c1a8e20b20ad79e6acb5bdc0c6fa012842b8ca
ziadbalbzioui-droid <ziadbalbzioui@gmail.com>
2026-01-21 14:12:32 +0100

    graphisme

--- interface/plateau.py
+++ interface/plateau.py
@@ -1,65 +1,135 @@
 import pyxel
-from PIL import Image
 
 class Plateau:
     def __init__(self):
+        # Activation de la souris
         pyxel.init(250, 250, title="Plateau d'échecs")
+        pyxel.mouse(True) 
+        
+        # Dimensions
         self.cell_size = 27
         self.board_size = self.cell_size * 8
-        total_size = self.board_size 
-        self.offset_x = (250- total_size) // 2 
-        self.offset_y = (250 - total_size) // 2
+        self.offset_x = (250 - self.board_size) // 2 
+        self.offset_y = (250 - self.board_size) // 2
+        
+        # --- ETAT DU JEU ---
         self.pieces = [[None for _ in range(8)] for _ in range(8)]
+        self.tour = 'w'       # 'w' pour les blancs, 'b' pour les noirs
+        self.selection = None # Stockera (ligne, col) de la pièce sélectionnée
+        
+        # Placement initial
         for i in range(8):
             self.pieces[1][i] = 'bP'  
             self.pieces[6][i] = 'wP' 
-        self.pieces[0][0] = self.pieces[0][7] = 'bR'
-        self.pieces[7][0] = self.pieces[7][7] = 'wR'
-        self.pieces[0][1] = self.pieces[0][6] = 'bN'
-        self.pieces[7][1] = self.pieces[7][6] = 'wN'
-        self.pieces[0][2] = self.pieces[0][5] = 'bB'
-        self.pieces[7][2] = self.pieces[7][5] = 'wB'
-        self.pieces[0][3] = 'bQ'
-        self.pieces[7][3] = 'wQ'
-        self.pieces[0][4] = 'bK'
-        self.pieces[7][4] = 'wK'
-        self.images = {}
-        self.load_images()
+        placement = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R']
+        for i, p in enumerate(placement):
+            self.pieces[0][i] = 'b' + p
+            self.pieces[7][i] = 'w' + p
+
+        # --- DESSINS (tes coordonnées) ---
+        self.modeles_pieces = {
+            "pion": [(7,3),(8,3),(6,4),(7,4),(8,4),(9,4),(5,5),(6,5),(7,5),(8,5),(9,5),(10,5),(6,6),(7,6),(8,6),(9,6),(7,7),(8,7),(5,8),(6,8),(7,8),(8,8),(9,8),(10,8),(5,9),(6,9),(7,9),(8,9),(9,9),(10,9)],
+            "tour": [(5, 2), (7, 2), (9, 2),(5, 3), (6, 3), (7, 3), (8, 3), (9, 3),(6, 4), (7, 4), (8, 4),(6, 5), (7, 5), (8, 5),(6, 6), (7, 6), (8, 6),(6, 7), (7, 7), (8, 7),(6, 8), (7, 8), (8, 8),(6, 9), (7, 9), (8, 9),(6, 10), (7, 10), (8, 10),(6, 11), (7, 11), (8, 11),(5, 12), (6, 12), (7, 12), (8, 12), (9, 12),(4, 13), (5, 13), (6, 13), (7, 13), (8, 13), (9, 13), (10, 13)],
+            "cavalier": [(7, 3),(6, 4), (7, 4), (8, 4),(5, 5), (6, 5), (7, 5), (8, 5), (9, 5),(5, 6), (6, 6), (7, 6), (8, 6), (9, 6),(10,6),(5, 7), (6, 7), (7, 7), (8, 7),(5, 8), (6, 8), (7, 8), (8, 8),(5, 9), (6, 9), (7, 9), (8, 9),(5, 10), (6, 10), (7, 10), (8, 10), (9, 10),(5, 11), (6, 11), (7, 11), (8, 11), (9, 11),(5, 12), (6, 12), (7, 12), (8, 12), (9, 12),(4, 13), (5, 13), (6, 13), (7, 13), (8, 13), (9, 13), (10, 13)],
+            "fou": [(7, 2),(6, 3), (7, 3), (8, 3),(6, 4), (7, 4), (8, 4),(5, 5), (6, 5), (7, 5), (8, 5), (9, 5),(5, 6), (6, 6), (7, 6), (8, 6), (9, 6),(6, 7), (7, 7), (8, 7),(6, 8), (7, 8), (8, 8),(6, 9), (7, 9), (8, 9),(6, 10), (7, 10), (8, 10),(6, 11), (7, 11), (8, 11),(5, 12), (6, 12), (7, 12), (8, 12), (9, 12),(4, 13), (5, 13), (6, 13), (7, 13), (8, 13), (9, 13), (10, 13)],
+            "dame": [(5, 2), (7, 2), (9, 2), (5, 3), (6, 3), (7, 3), (8, 3), (9, 3),(6, 4), (7, 4), (8, 4), (5, 5), (6, 5), (7, 5), (8, 5), (9, 5),(6, 6), (7, 6), (8, 6), (6, 7), (7, 7), (8, 7), (6, 8), (7, 8), (8, 8),(6, 9), (7, 9), (8, 9), (6, 10), (7, 10), (8, 10), (6, 11), (7, 11), (8, 11),(5, 12), (6, 12), (7, 12), (8, 12), (9, 12),(4, 13), (5, 13), (6, 13), (7, 13), (8, 13), (9, 13), (10, 13)],
+            "roi": [(7, 0),(6, 1), (7, 1), (8, 1),(7, 2),(6, 3), (7, 3), (8, 3),(5, 4), (6, 4), (7, 4), (8, 4), (9, 4),(6, 5), (7, 5), (8, 5),(6, 6), (7, 6), (8, 6),(6, 7), (7, 7), (8, 7),(6, 8), (7, 8), (8, 8),(6, 9), (7, 9), (8, 9),(6, 10), (7, 10), (8, 10),(6, 11), (7, 11), (8, 11),(5, 12), (6, 12), (7, 12), (8, 12), (9, 12),(4, 13), (5, 13), (6, 13), (7, 13), (8, 13), (9, 13), (10, 13)]
+        }
+        self.map_noms = {'P': 'pion', 'R': 'tour', 'N': 'cavalier', 'B': 'fou', 'Q': 'dame', 'K': 'roi'}
+
         pyxel.run(self.update, self.draw)
 
-    def load_images(self):
-        pieces_files = ["bP","wP","bR","wR","bN","wN","bB","wB","bQ","wQ","bK","wK"]
-        for name in pieces_files:
-            img = Image.open(f"images/{name}.png").resize((self.cell_size, self.cell_size))
-            self.images[name] = pyxel.image(0).load(0, 0, img)    
-    
     def update(self):
-        pass
+        # Détection du clic gauche
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            mx = pyxel.mouse_x
+            my = pyxel.mouse_y
+            
+            # Conversion pixels -> coordonnées grille (0-7)
+            col = (mx - self.offset_x) // self.cell_size
+            ligne = (my - self.offset_y) // self.cell_size
+            
+            # Vérifier si on a cliqué DANS le plateau
+            if 0 <= col < 8 and 0 <= ligne < 8:
+                contenu_case = self.pieces[ligne][col]
+                
+                # CAS 1 : Rien n'est sélectionné -> On essaie de sélectionner
+                if self.selection is None:
+                    if contenu_case is not None:
+                        # On vérifie que la pièce appartient au joueur dont c'est le tour
+                        if contenu_case[0] == self.tour:
+                            self.selection = (ligne, col)
+                
+                # CAS 2 : Une pièce est déjà sélectionnée -> On essaie de bouger
+                else:
+                    sel_l, sel_c = self.selection
+                    piece_selectionnee = self.pieces[sel_l][sel_c]
+                    
+                    # Si on clique sur la MEME case -> on désélectionne
+                    if (ligne, col) == (sel_l, sel_c):
+                        self.selection = None
+                    
+                    # Si on clique sur une pièce AMIE -> on change la sélection
+                    elif contenu_case is not None and contenu_case[0] == self.tour:
+                        self.selection = (ligne, col)
+                        
+                    # Sinon -> C'est un mouvement (case vide ou ennemie)
+                    else:
+                        # 1. On déplace la pièce
+                        self.pieces[ligne][col] = piece_selectionnee
+                        self.pieces[sel_l][sel_c] = None
+                        
+                        # 2. On change de tour
+                        self.tour = 'b' if self.tour == 'w' else 'w'
+                        
+                        # 3. On désélectionne
+                        self.selection = None
 
     def draw(self):
         pyxel.cls(13)
+        
+        # --- Dessin du damier et des pièces ---
         for ligne in range(8):
             for col in range(8):
-                x = self.offset_x + col * self.cell_size
-                y = self.offset_y + ligne * self.cell_size
+                x_case = self.offset_x + col * self.cell_size
+                y_case = self.offset_y + ligne * self.cell_size
 
-                color = 7 if (ligne + col) % 2 == 0 else 0
-                pyxel.rect(x, y, self.cell_size, self.cell_size, color)
+                # Couleur du sol
+                couleur_sol = 6 if (ligne + col) % 2 == 0 else 1
+                
+                # Si la case est SÉLECTIONNÉE, on la met en jaune (10)
+                if self.selection == (ligne, col):
+                    couleur_sol = 10
+                
+                pyxel.rect(x_case, y_case, self.cell_size, self.cell_size, couleur_sol)
 
-                piece = self.pieces[ligne][col]
-                if piece:
-                    pyxel.blt(x, y, 0, 0, 0, self.cell_size, self.cell_size, 0)
+                # Dessin de la pièce
+                code_piece = self.pieces[ligne][col]
+                if code_piece:
+                    couleur_p = code_piece[0]
+                    type_p = code_piece[1]
+                    col_pixel = 7 if couleur_p == 'w' else 0
+                    
+                    pixels = self.modeles_pieces[self.map_noms[type_p]]
+                    for px, py in pixels:
+                        pyxel.pset(x_case + px + 6, y_case + py + 6, col_pixel)
 
+        # --- Interface (Qui doit jouer ?) ---
+        msg = "Tour: BLANCS" if self.tour == 'w' else "Tour: NOIRS"
+        col_txt = 7 if self.tour == 'w' else 0
+        pyxel.text(5, 5, msg, col_txt)
+        
+        # Coordonnées
         for col in range(8):
             lettre = chr(ord("A") + col)
-            x = self.offset_x + col * self.cell_size + 9
+            x = self.offset_x + col * self.cell_size + 11
             y = self.offset_y + 8 * self.cell_size + 5
             pyxel.text(x, y, lettre, 0)
 
         for ligne in range(8):
             nb = str(8 - ligne)
-            x = self.offset_x - 15
-            y = self.offset_y + ligne * self.cell_size + 9
+            x = self.offset_x - 10
+            y = self.offset_y + ligne * self.cell_size + 11
             pyxel.text(x, y, nb, 0)
 
 Plateau()
